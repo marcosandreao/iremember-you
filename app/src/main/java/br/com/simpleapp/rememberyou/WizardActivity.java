66ccea4432fe94f3f714f97aa1842c4e9e61b4f5
Marcos Oliveira <marcosandreao@gmail.com>
2016-05-05 22:56:13 -0400

    adicionado toolbar na tela inicial

--- app/src/main/java/br/com/simpleapp/rememberyou/WizardActivity.java
+++ app/src/main/java/br/com/simpleapp/rememberyou/WizardActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +36,15 @@ public class WizardActivity extends AppCompatActivity implements IWizardListener
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.activity_wizard);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.welcome_title);
+        this.setSupportActionBar(toolbar);
+
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
 
         this.getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new StepOneFragment()).commit();
     }
